285fcfda4341e3e1363bfa63f97b5d2bba233396
David Wilson <david@daviwil.com>
2022-04-03 10:39:04 +0300

    window: Turn off GLFW_OPENGL_FORWARD_COMPAT

--- src/window.c
+++ src/window.c
@@ -21,7 +21,7 @@ SubstWindow *subst_window_create(int width, int height, const char *title) {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);
 
 #ifdef __APPLE__
   // Just in case...
