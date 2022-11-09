2e5ac50adec1fbd1aed53a8a178e38c8a34006cc
David Wilson <david@daviwil.com>
2022-11-09 15:22:18 +0200

    Remove game-specific code before splitting out to `mesche window`

--- src/lib.c
+++ src/lib.c
@@ -1,6 +1,4 @@
 #include "font.h"
-#include "particle.h"
-#include "physics.h"
 #include "renderer.h"
 #include "texture.h"
 #include "window.h"
@@ -13,6 +11,4 @@ void substratic_library_init(VM *vm) {
   subst_window_module_init(vm);
   subst_texture_module_init(vm);
   subst_renderer_module_init(vm);
-  subst_physics_module_init(vm);
-  subst_particle_module_init(vm);
 }
--- src/particle.h
+++
@@ -1,6 +0,0 @@
-#ifndef __subst_particle_h
-#define __subst_particle_h
-
-void subst_particle_module_init(VM *vm);
-
-#endif
